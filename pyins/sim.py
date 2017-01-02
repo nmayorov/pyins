d7db471d8dfd2dfa8441a9b2a9098ec4af32855d
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2017-01-02 22:01:34 +0500

    API, ENH: Update in sim
    Now only two primitive functions are available from_position and from_velocity
    which require values at consecutive time stamps

--- pyins/sim.py
+++ pyins/sim.py
@@ -1,143 +1,12 @@
-"""Strapdown sensor simulator."""
+"""Strapdown sensors simulator."""
 import numpy as np
 import pandas as pd
-from scipy.interpolate import CubicSpline, interp1d
-from . import dcm, earth, coord
-from .util import mm_prod, mv_prod
+from scipy.interpolate import CubicSpline, PPoly
+from scipy.linalg import solve_banded
+from . import dcm, earth, coord, util
 
 
-def _interpolate_ll(lat, lon, t, t_out):
-    Cen = dcm.from_llw(lat, lon)
-    dCen = mm_prod(Cen[:-1], Cen[1:], at=True)
-    rv = dcm.to_rv(dCen)
-    rv_acc = np.vstack((np.zeros(3), np.cumsum(rv, axis=0)))
-    rv_acc_interp = interp1d(t, rv_acc, axis=0)
-    rv_acc_out = rv_acc_interp(t_out)
-
-    Cen_out = []
-    for i in range(t.shape[0] - 1):
-        ind = (t_out >= t[i]) & (t_out < t[i + 1])
-        rv_i = rv_acc_out[ind] - rv_acc[i]
-        dC = dcm.from_rv(rv_i)
-        Cen_out.append(mm_prod(Cen[i], dC))
-
-    Cen_out = np.vstack(Cen_out)
-    lat, lon, _ = dcm.to_llw(Cen_out)
-    return lat, lon
-
-
-def _interpolate_hpr(h, p, r, t, t_out):
-    C = dcm.from_hpr(h, p, r)
-    dCen = mm_prod(C[:-1], C[1:], at=True)
-    rv = dcm.to_rv(dCen)
-    rv_acc = np.vstack((np.zeros(3), np.cumsum(rv, axis=0)))
-    rv_acc_interp = interp1d(t, rv_acc, axis=0)
-    rv_acc_out = rv_acc_interp(t_out)
-
-    C_out = []
-    for i in range(t.shape[0] - 1):
-        ind = (t_out >= t[i]) & (t_out < t[i + 1])
-        rv_i = rv_acc_out[ind] - rv_acc[i]
-        dC = dcm.from_rv(rv_i)
-        C_out.append(mm_prod(C[i], dC))
-
-    C_out = np.vstack(C_out)
-    return dcm.to_hpr(C_out)
-
-
-def from_position(dt, lat, lon, t=None, h=None, p=None, r=None, alt=None):
-    """Generate inertial readings given position and optionally attitude.
-
-    Parameters
-    ----------
-    dt : float
-        Desired sensor sampling period.
-    lat, lon : array_like, shape (n_points,)
-        Time series of latitude and longitude.
-    t : array_like with shape (n_points,) or None, optional
-        If array, time stamps corresponding to `lat` and `lon`. If None
-        (default), then `lat` and `lon` are assumed to be spaced by `dt`
-    h, p, r : array_like with shape (n_points,) or None
-        Time series of heading, pitch and roll angles. If (None) default,
-        heading and pitch will be computed assuming that the velocity is
-        directed along the body longitudinal axis, and roll will be set to 0.
-        This is not going to work if the system is stationary during some
-        period (to be redesigned). Note that `h`, `p`, `r` must be all set or
-        left as None.
-    alt : array_like with shape (n_points,) or None, optional
-        Time series of altitude. If None (default), then altitude is assumed
-        to be 0 all the time.
-
-    Returns
-    -------
-    traj : DataFrame
-        Trajectory. Contains n_readings + 1 rows.
-    gyro : ndarray, shape (n_readings, 3)
-        Gyro readings.
-    accel : ndarray, shape (n_readings, 3)
-        Accelerometer readings.
-    """
-    test = (h is None) + (p is None) + (r is None)
-    if test != 0 and test != 3:
-        raise ValueError("`h`, `p`, `r` must be all set or left as None.")
-
-    need_interpolation = t is not None
-    compute_attitude = test == 3
-
-    if t is None:
-        t = np.arange(lat.shape[0]) * dt
-        t_out = t
-    else:
-        t = np.asarray(t)
-        t_out = np.arange(t[0], t[-1], dt)
-
-    if np.any(np.diff(t) <= 0):
-        raise ValueError("`t` must be strictly increasing.")
-
-    if alt is None:
-        alt = np.zeros_like(t_out)
-
-    if need_interpolation:
-        lat, lon = _interpolate_ll(lat, lon, t, t_out)
-
-        if not compute_attitude:
-            h, p, r = _interpolate_hpr(h, p, r, t, t_out)
-
-    return _generate_sensors(dt, lat, lon, alt, h, p, r)
-
-
-def _generate_sensors(dt, lat, lon, alt, h, p, r):
-    time = dt * np.arange(lat.shape[0])
-    lat_inertial = lat.copy()
-    lon_inertial = lon.copy()
-    lon_inertial += np.rad2deg(earth.RATE) * time
-    Cin = dcm.from_llw(lat_inertial, lon_inertial)
-
-    R = coord.lla_to_ecef(lat_inertial, lon_inertial, alt)
-    v_s = CubicSpline(time, R).derivative()
-    v = v_s(time)
-
-    V = v.copy()
-    V[:, 0] += earth.RATE * R[:, 1]
-    V[:, 1] -= earth.RATE * R[:, 0]
-    V = mv_prod(Cin, V, at=True)
-
-    if h is None:
-        h = np.rad2deg(np.arctan2(V[:, 0], V[:, 1]))
-
-    if p is None:
-        p = np.rad2deg(np.arctan2(V[:, 2], np.hypot(V[:, 0], V[:, 1])))
-
-    if r is None:
-        r = np.zeros_like(p)
-
-    Cnb = dcm.from_hpr(h, p, r)
-    Cib = mm_prod(Cin, Cnb)
-
-    Cib_spline = dcm.Spline(time, Cib)
-    a = Cib_spline.c[2]
-    b = Cib_spline.c[1]
-    c = Cib_spline.c[0]
+def _compute_readings(dt, a, b, c, d, e):
     ab = np.cross(a, b)
     ac = np.cross(a, c)
     bc = np.cross(b, c)
@@ -157,13 +26,6 @@ def _generate_sensors(dt, lat, lon, alt, h, p, r):
         gyros += omega[k] / (k + 1)
         gyros *= dt
 
-    g = earth.gravitation_ecef(lat_inertial, lon_inertial)
-    a_s = v_s.derivative()
-    d = a_s.c[1] - g[:-1]
-    e = a_s.c[0] - np.diff(g, axis=0) / dt
-
-    d = mv_prod(Cib[:-1], d, at=True)
-    e = mv_prod(Cib[:-1], e, at=True)
     ad = np.cross(a, d)
     ae = np.cross(a, e)
     bd = np.cross(b, d)
@@ -188,11 +50,207 @@ def _generate_sensors(dt, lat, lon, alt, h, p, r):
         accels += f[k] / (k + 1)
         accels *= dt
 
+    return gyros, accels
+
+
+def from_position(dt, lat, lon, alt, h, p, r):
+    """Generate inertial readings given position and attitude.
+
+    Parameters
+    ----------
+    dt : float
+        Time step.
+    lat, lon, alt : array_like, shape (n_points,)
+        Time series of latitude, longitude and altitude.
+    h, p, r : array_like, shape (n_points,)
+        Time series of heading, pitch and roll angles.
+
+    Returns
+    -------
+    traj : DataFrame
+        Trajectory. Contains n_points rows.
+    gyro : ndarray, shape (n_points - 1, 3)
+        Gyro readings.
+    accel : ndarray, shape (n_points - 1, 3)
+        Accelerometer readings.
+    """
+    lat = np.asarray(lat, dtype=float)
+    lon = np.asarray(lon, dtype=float)
+    alt = np.asarray(alt, dtype=float)
+    h = np.asarray(h, dtype=float)
+    p = np.asarray(p, dtype=float)
+    r = np.asarray(r, dtype=float)
+    n_points = lat.shape[0]
+
+    time = dt * np.arange(n_points)
+    lat_inertial = lat.copy()
+    lon_inertial = lon.copy()
+    lon_inertial += np.rad2deg(earth.RATE) * time
+    Cin = dcm.from_llw(lat_inertial, lon_inertial)
+
+    R = coord.lla_to_ecef(lat_inertial, lon_inertial, alt)
+    v_s = CubicSpline(time, R).derivative()
+    v = v_s(time)
+
+    V = v.copy()
+    V[:, 0] += earth.RATE * R[:, 1]
+    V[:, 1] -= earth.RATE * R[:, 0]
+    V = util.mv_prod(Cin, V, at=True)
+
+    Cnb = dcm.from_hpr(h, p, r)
+    Cib = util.mm_prod(Cin, Cnb)
+
+    Cib_spline = dcm.Spline(time, Cib)
+    a = Cib_spline.c[2]
+    b = Cib_spline.c[1]
+    c = Cib_spline.c[0]
+
+    g = earth.gravitation_ecef(lat_inertial, lon_inertial)
+    a_s = v_s.derivative()
+    d = a_s.c[1] - g[:-1]
+    e = a_s.c[0] - np.diff(g, axis=0) / dt
+
+    d = util.mv_prod(Cib[:-1], d, at=True)
+    e = util.mv_prod(Cib[:-1], e, at=True)
+
+    gyros, accels = _compute_readings(dt, a, b, c, d, e)
+
     traj = pd.DataFrame(index=np.arange(time.shape[0]))
     traj['lat'] = lat
     traj['lon'] = lon
     traj['VE'] = V[:, 0]
     traj['VN'] = V[:, 1]
+    traj['h'] = h
+    traj['p'] = p
+    traj['r'] = r
+
+    return traj, gyros, accels
+
+
+class _QuadraticSpline(PPoly):
+    def __init__(self, x, y):
+        x = np.asarray(x, dtype=float)
+        y = np.asarray(y, dtype=float)
+
+        n = x.shape[0]
+        dx = np.diff(x)
+        dy = np.diff(y, axis=0)
+        dxr = dx.reshape([dx.shape[0]] + [1] * (y.ndim - 1))
+
+        c = np.empty((3, n - 1) + y.shape[1:])
+        if n > 2:
+            A = np.ones((2, n))
+            b = np.empty((n,) + y.shape[1:])
+            b[0] = 0
+            b[1:] = 2 * dy / dxr
+            s = solve_banded((1, 0), A, b, overwrite_ab=True, overwrite_b=True,
+                             check_finite=False)
+            c[0] = np.diff(s, axis=0) / (2 * dxr)
+            c[1] = s[:-1]
+            c[2] = y[:-1]
+        else:
+            c[0] = 0
+            c[1] = dy / dxr
+            c[2] = y[:-1]
+
+        super(_QuadraticSpline, self).__init__(c, x)
+
+
+def from_velocity(dt, lat0, lon0, alt0, VE, VN, VU, h, p, r):
+    """Generate inertial readings given velocity and attitude.
+
+    Parameters
+    ----------
+    dt : float
+        Time step.
+    lat0, lon0, alt0 : float
+        Initial values of latitude, longitude and altitude.
+    VE, VN, VU : array_like, shape (n_points,)
+        Time series of East, North and vertical velocity components.
+    h, p, r : array_like, shape (n_points,)
+        Time series of heading, pitch and roll angles.
+
+    Returns
+    -------
+    traj : DataFrame
+        Trajectory. Contains n_points rows.
+    gyro : ndarray, shape (n_points - 1, 3)
+        Gyro readings.
+    accel : ndarray, shape (n_points - 1, 3)
+        Accelerometer readings.
+    """
+    MAX_ITER = 3
+    ACCURACY = 0.01
+
+    VE = np.asarray(VE, dtype=float)
+    VN = np.asarray(VN, dtype=float)
+    VU = np.asarray(VU, dtype=float)
+    h = np.asarray(h, dtype=float)
+    p = np.asarray(p, dtype=float)
+    r = np.asarray(r, dtype=float)
+    n_points = VE.shape[0]
+    time = np.arange(n_points) * dt
+
+    VU_spline = _QuadraticSpline(time, VU)
+    alt_spline = VU_spline.antiderivative()
+    alt = alt0 + alt_spline(time)
+
+    lat0 = np.deg2rad(lat0)
+    lon0 = np.deg2rad(lon0)
+    lat = lat0
+
+    for iteration in range(MAX_ITER):
+        _, rn = earth.principal_radii(np.sin(lat))
+        rn += alt
+        dlat_spline = _QuadraticSpline(time, VN / rn)
+        lat_spline = dlat_spline.antiderivative()
+        lat_new = lat_spline(time) + lat0
+        delta = (lat - lat_new) * rn
+        lat = lat_new
+        if np.all(np.abs(delta) < ACCURACY):
+            break
+
+    re, _ = earth.principal_radii(np.sin(lat))
+    re += alt
+    dlon_spline = _QuadraticSpline(time, VE / (re * np.cos(lat)))
+    lon_spline = dlon_spline.antiderivative()
+    lon = lon_spline(time) + lon0
+
+    lat = np.rad2deg(lat)
+    lon = np.rad2deg(lon)
+    lon_inertial = lon + np.rad2deg(earth.RATE) * time
+    Cin = dcm.from_llw(lat, lon_inertial)
+
+    v = np.vstack((VE, VN, VU)).T
+    v = util.mv_prod(Cin, v)
+    R = coord.lla_to_ecef(lat, lon_inertial, alt)
+    v[:, 0] -= earth.RATE * R[:, 1]
+    v[:, 1] += earth.RATE * R[:, 0]
+    v_s = _QuadraticSpline(time, v)
+
+    Cnb = dcm.from_hpr(h, p, r)
+    Cib = util.mm_prod(Cin, Cnb)
+
+    Cib_spline = dcm.Spline(time, Cib)
+    a = Cib_spline.c[2]
+    b = Cib_spline.c[1]
+    c = Cib_spline.c[0]
+
+    g = earth.gravitation_ecef(lat, lon_inertial)
+    a_s = v_s.derivative()
+    d = a_s.c[1] - g[:-1]
+    e = a_s.c[0] - np.diff(g, axis=0) / dt
+
+    d = util.mv_prod(Cib[:-1], d, at=True)
+    e = util.mv_prod(Cib[:-1], e, at=True)
+
+    gyros, accels = _compute_readings(dt, a, b, c, d, e)
+
+    traj = pd.DataFrame(index=np.arange(n_points))
+    traj['lat'] = lat
+    traj['lon'] = lon
+    traj['VE'] = VE
+    traj['VN'] = VN
     traj['h'] = h
     traj['p'] = p
     traj['r'] = r
--- pyins/tests/test_sim.py
+++ pyins/tests/test_sim.py
@@ -1,13 +1,22 @@
 import numpy as np
 from numpy.testing import assert_allclose, run_module_suite
-from pyins.sim import from_position
+from pyins import sim
 from pyins import earth
 
 
 def test_from_position():
-    lat = [50, 50]
-    lon = [45, 45]
     dt = 1e-1
+    n_points = 1000
+
+    lat = np.full(n_points, 50.0)
+    lon = np.full(n_points, 45.0)
+    alt = np.zeros(n_points)
+    h = np.zeros(n_points)
+    p = np.zeros(n_points)
+    r = np.zeros(n_points)
+    VE = np.zeros(n_points)
+    VN = np.zeros(n_points)
+    VU = np.zeros(n_points)
 
     slat = np.sin(np.deg2rad(50))
     clat = (1 - slat**2) ** 0.5
@@ -15,8 +24,21 @@ def test_from_position():
     gyro = earth.RATE * np.array([0, clat, slat]) * dt
     accel = np.array([0, 0, earth.gravity(slat)]) * dt
 
-    traj, gyro_g, accel_g = from_position(dt, lat, lon, [0, 10],
-                                          h=[0, 0], p=[0, 0], r=[0, 0])
+    traj, gyro_g, accel_g = sim.from_position(dt, lat, lon, alt, h, p, r)
+    assert_allclose(traj.lat, 50, rtol=1e-12)
+    assert_allclose(traj.lon, 45, rtol=1e-12)
+    assert_allclose(traj.VE, 0, atol=1e-7)
+    assert_allclose(traj.VN, 0, atol=1e-7)
+    assert_allclose(traj.h, 0, atol=1e-8)
+    assert_allclose(traj.p, 0, atol=1e-8)
+    assert_allclose(traj.r, 0, atol=1e-8)
+
+    for i in range(3):
+        assert_allclose(gyro_g[:, i], gyro[i], atol=1e-16)
+        assert_allclose(accel_g[:, i], accel[i], atol=1e-7)
+
+    traj, gyro_g, accel_g = sim.from_velocity(dt, 50, 45, 0, VE, VN, VU,
+                                              h, p, r)
     assert_allclose(traj.lat, 50, rtol=1e-12)
     assert_allclose(traj.lon, 45, rtol=1e-12)
     assert_allclose(traj.VE, 0, atol=1e-7)
