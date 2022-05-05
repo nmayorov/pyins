BUG: Fix computation of accel readings in test_integrate.py

pyins/tests/test_integrate.py
@@ -33,7 +33,7 @@ def test_integrate():
     gyro = Cnb.T.dot(gyro)
     gyro = np.resize(gyro, (n, 3))
 
-    accel = np.array([0, 0, earth.gravity(1/np.sqrt(2))]) * dt
+    accel = np.array([0, 0, earth.gravity(45)]) * dt
     accel = Cnb.T.dot(accel)
     accel = np.resize(accel, (n, 3))
 
@@ -63,7 +63,7 @@ def test_integrate_rate_sensors():
     gyro = Cnb.T.dot(gyro)
     gyro = np.resize(gyro, (n, 3))
 
-    accel = np.array([0, 0, earth.gravity(1/np.sqrt(2))])
+    accel = np.array([0, 0, earth.gravity(45)])
     accel = Cnb.T.dot(accel)
     accel = np.resize(accel, (n, 3))
 