BUG: Fix pitch computation in sim.constant_velocity_motion

pyins/sim.py
@@ -4,6 +4,7 @@ import pandas as pd
 from scipy.interpolate import CubicSpline, PPoly
 from scipy.linalg import solve_banded
 from scipy.spatial.transform import Rotation, RotationSpline
+from scipy._lib._util import check_random_state
 from . import dcm, earth, transform, util
 
 
@@ -271,7 +272,7 @@ def constant_velocity_motion(dt, total_time, lla0, velocity_n,
     velocity_n = np.tile(velocity_n, (n_points, 1))
     rph = np.zeros_like(velocity_n)
     rph[:, 1] = np.rad2deg(np.arctan2(
-        np.hypot(velocity_n[:, 0], velocity_n[:, 1]), velocity_n[:, 2]))
+        velocity_n[:, 2], np.hypot(velocity_n[:, 0], velocity_n[:, 1])))
     rph[:, 2] = np.rad2deg(np.arctan2(velocity_n[:, 0], velocity_n[:, 1]))
     return from_velocity(dt, lla0, velocity_n, rph, sensor_type)
 