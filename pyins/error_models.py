69bbdbb25f0d4cc9105d9ee210eca8cbe0784827
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2022-05-09 19:03:08 +0500

    ENH: Implement ModifiedPsiModel

--- pyins/error_models.py
+++ pyins/error_models.py
@@ -220,6 +220,113 @@ class ModifiedPhiModel(ErrorModel):
         return result
 
 
+class ModifiedPsiModel(ErrorModel):
+    DR1 = 0
+    DR2 = 1
+    DR3 = 2
+    DV1 = 3
+    DV2 = 4
+    DV3 = 5
+    PSI1 = 6
+    PSI2 = 7
+    PSI3 = 8
+
+    DR = [DR1, DR2, DR3]
+    DV = [DV1, DV2, DV3]
+    PSI = [PSI1, PSI2, PSI3]
+
+    STATES = OrderedDict(DR1=DR1, DR2=DR2, DR3=DR3,
+                         DV1=DV1, DV2=DV2, DV3=DV3,
+                         PSI1=PSI1, PSI2=PSI1, PSI3=PSI1)
+
+    def system_matrix(self, trajectory):
+        n_samples = trajectory.shape[0]
+
+        V_skew = dcm.skew_matrix(trajectory[['VE', 'VN', 'VU']])
+        R = earth.curvature_matrix(trajectory.lat, trajectory.alt)
+        Omega_n = earth.rate_n(trajectory.lat)
+        rho_n = util.mv_prod(R, trajectory[['VE', 'VN', 'VU']])
+        g_n_skew = dcm.skew_matrix(earth.gravity_n(trajectory.lat,
+                                                   trajectory.alt))
+        Cnb = dcm.from_rph(trajectory[['roll', 'pitch', 'heading']])
+
+        F = np.zeros((n_samples, self.N_STATES, self.N_STATES))
+        samples = np.arange(n_samples)
+
+        F[np.ix_(samples, self.DR, self.DV)] = np.eye(3)
+        F[np.ix_(samples, self.DR, self.PSI)] = V_skew
+
+        F[np.ix_(samples, self.DV, self.DV)] = -dcm.skew_matrix(
+            2 * Omega_n + rho_n)
+        F[np.ix_(samples, self.DV, self.DR)] = -g_n_skew @ R
+        F[np.ix_(samples, self.DV, self.PSI)] = -g_n_skew
+        F[:, self.DV3, self.DR3] += 2 * earth.gravity(trajectory.lat,
+                                                      trajectory.alt) / earth.R0
+
+        F[np.ix_(samples, self.PSI, self.PSI)] = -dcm.skew_matrix(rho_n +
+                                                                  Omega_n)
+
+        B_gyro = np.zeros((n_samples, self.N_STATES, 3))
+        B_gyro[np.ix_(samples, self.DV, [0, 1, 2])] = util.mm_prod(V_skew, Cnb)
+        B_gyro[np.ix_(samples, self.PSI, [0, 1, 2])] = -Cnb
+
+        B_accel = np.zeros((n_samples, self.N_STATES, 3))
+        B_accel[np.ix_(samples, self.DV, [0, 1, 2])] = Cnb
+
+        return F, B_gyro, B_accel
+
+    def transform_to_output(self, trajectory):
+        series = isinstance(trajectory, pd.Series)
+        if series:
+            trajectory = trajectory.to_frame().transpose()
+
+        T = np.zeros((trajectory.shape[0], self.N_STATES, self.N_STATES))
+        samples = np.arange(len(trajectory))
+        T[np.ix_(samples, self.DR_OUT, self.DR)] = np.eye(3)
+        T[np.ix_(samples, self.DV_OUT, self.DV)] = np.eye(3)
+        T[np.ix_(samples, self.DV_OUT, self.PSI)] = dcm.skew_matrix(
+            trajectory[['VE', 'VN', 'VU']])
+
+        T_rph_phi = transform.phi_to_delta_rph(
+            trajectory[['roll', 'pitch', 'heading']])
+        R = earth.curvature_matrix(trajectory.lat, trajectory.alt)
+        T[np.ix_(samples, self.DRPH, self.PSI)] = T_rph_phi
+        T[np.ix_(samples, self.DRPH, self.DR)] = util.mm_prod(T_rph_phi, R)
+
+        return T[0] if series else T
+
+    def correct_state(self, trajectory_point, error):
+        R = earth.curvature_matrix(trajectory_point.lat, trajectory_point.alt)
+        Ctp = dcm.from_rv(error[self.PSI] + R @ error[self.DR])
+        lla = transform.perturb_lla(trajectory_point[['lat', 'lon', 'alt']],
+                                    -error[self.DR])
+        velocity_n = Ctp @ (trajectory_point[['VE', 'VN', 'VU']]
+                            - error[self.DV])
+        rph = dcm.to_rph(
+            Ctp @ dcm.from_rph(trajectory_point[['roll', 'pitch', 'heading']]))
+
+        return pd.Series(data=np.hstack((lla, velocity_n, rph)),
+                         index=trajectory_point.index)
+
+    def position_error_jacobian(self, trajectory_point):
+        result = np.zeros((3, self.N_STATES))
+        result[:, self.DR] = np.eye(3)
+        return result
+
+    def enu_velocity_error_jacobian(self, trajectory_point):
+        result = np.zeros((3, self.N_STATES))
+        result[:, self.DV] = np.eye(3)
+        result[:, self.PSI] = dcm.skew_matrix(
+            trajectory_point[['VE', 'VN', 'VU']])
+        return result
+
+    def body_velocity_error_jacobian(self, trajectory_point):
+        Cnb = dcm.from_rph(trajectory_point[['roll', 'pitch', 'heading']])
+        result = np.zeros((3, self.N_STATES))
+        result[:, self.DV] = Cnb.transpose()
+        return result
+
+
 def propagate_errors(dt, traj,
                      delta_position_n=np.zeros(3),
                      delta_velocity_n=np.zeros(3),
