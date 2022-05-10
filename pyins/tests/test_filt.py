95b481ed5361a850c3f73ed3df4dc3a8d129f5d5
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2022-05-11 01:12:52 +0500

    TST: Add solid test for FeedforwardFilter

--- pyins/tests/test_filt.py
+++ pyins/tests/test_filt.py
@@ -178,65 +178,89 @@ def test_FeedbackFilter():
 
 
 def test_FeedforwardFilter():
-    # Test that the results are reasonable on a static bench.
-    dt = 1
-    trajectory = pd.DataFrame(index=np.arange(1 * 3600))
-    trajectory['lat'] = 50
-    trajectory['lon'] = 60
-    trajectory['alt'] = 100
-    trajectory['VN'] = 0
-    trajectory['VE'] = 0
-    trajectory['VD'] = 0
-    trajectory['roll'] = 0
-    trajectory['pitch'] = 0
-    trajectory['heading'] = 0
+    dt = 0.01
+    rng = np.random.RandomState(0)
 
-    np.random.seed(1)
-    obs_data = pd.DataFrame(index=trajectory.index[::10])
-    obs_data[['lat', 'lon', 'alt']] = perturb_lla(
-        trajectory.loc[::10, ['lat', 'lon', 'alt']],
-        10 * np.random.randn(len(obs_data), 3))
-    position_obs = PositionObs(obs_data, 10)
+    trajectory, gyro_true, accel_true = sim.sinusoid_velocity_motion(
+        dt, 300, [50, 60, 100], [1, -1, 0.5], [3, 3, 0.5])
 
-    delta_position_n = [-3, 5, 0]
-    delta_velocity_n = [1, -1, 0]
-    delta_rph = [-0.02, 0.03, 0.1]
+    position_obs = filt.PositionObs(
+        sim.generate_position_observations(trajectory.iloc[::100], 1, rng), 1)
+    ned_velocity_obs = filt.NedVelocityObs(
+        sim.generate_ned_velocity_observations(trajectory.iloc[30::100], 0.5,
+                                               rng), 0.5)
+    body_velocity_obs = filt.BodyVelocityObs(
+        sim.generate_body_velocity_observations(trajectory.iloc[60::100], 0.2,
+                                                rng), 0.2)
 
-    errors, _ = propagate_errors(dt, trajectory, delta_position_n,
-                                 delta_velocity_n, delta_rph)
-    traj_error = correct_trajectory(trajectory, -errors)
+    imu_errors = sim.ImuErrors(
+        gyro_bias=np.array([-1, 2, 1]) * transform.DH_TO_RS,
+        gyro_noise=0.001 * transform.DRH_TO_RRS,
+        accel_bias=[0.01, -0.01, 0.02],
+        accel_noise=0.01 / 60,
+        rng=rng)
 
-    f = FeedforwardFilter(dt, trajectory, 5, 1, 0.2, 0.05)
-    res = f.run(traj_error, [position_obs])
+    gyro_model = filt.InertialSensor(bias=1 * transform.DH_TO_RS,
+                                     noise=0.001 * transform.DRH_TO_RRS)
+    accel_model = filt.InertialSensor(bias=0.01, noise=0.01 / 60)
 
-    x = errors.loc[3000:]
-    y = res.error.loc[3000:]
+    gyro, accel = imu_errors.apply(dt, gyro_true, accel_true)
+    theta, dv = strapdown.compute_theta_and_dv(gyro, accel)
 
-    assert_allclose(x.east, y.east, rtol=0, atol=10)
-    assert_allclose(x.north, y.north, rtol=0, atol=10)
-    assert_allclose(x.VE, y.VE, rtol=0, atol=2e-2)
-    assert_allclose(x.VN, y.VN, rtol=0, atol=2e-2)
-    assert_allclose(x.roll, y.roll, rtol=0, atol=1e-4)
-    assert_allclose(x.pitch, y.pitch, rtol=0, atol=1e-4)
-    assert_allclose(x.heading, y.heading, rtol=0, atol=1.5e-3)
-    assert_(np.all(np.abs(res.residuals[0] < 4)))
+    pos_sd = 10
+    vel_sd = 0.1
+    level_sd = 0.05
+    azimuth_sd = 0.2
 
-    res = f.run_smoother(traj_error, [position_obs])
+    lla, velocity_n, rph = sim.perturb_navigation_state(
+        trajectory.loc[0, ['lat', 'lon', 'alt']],
+        trajectory.loc[0, ['VN', 'VE', 'VD']],
+        trajectory.loc[0, ['roll', 'pitch', 'heading']],
+        pos_sd, vel_sd, level_sd, azimuth_sd,
+        rng=rng)
 
-    # This smoother we don't need to wait until the filter converges,
-    # the estimation accuracy is also improved some
-    x = errors
-    y = res.error
+    integrator = strapdown.StrapdownIntegrator(dt, lla, velocity_n, rph)
+    trajectory_computed = integrator.integrate(theta, dv)
 
-    assert_allclose(x.east, y.east, rtol=0, atol=10)
-    assert_allclose(x.north, y.north, rtol=0, atol=10)
-    assert_allclose(x.VE, y.VE, rtol=0, atol=2e-2)
-    assert_allclose(x.VN, y.VN, rtol=0, atol=2e-2)
-    assert_allclose(x.roll, y.roll, rtol=0, atol=1e-4)
-    assert_allclose(x.pitch, y.pitch, rtol=0, atol=1e-4)
-    assert_allclose(x.heading, y.heading, rtol=0, atol=1.5e-3)
-    assert_(np.all(np.abs(res.residuals[0] < 4)))
+    f = filt.FeedforwardFilter(dt, trajectory, pos_sd=pos_sd, vel_sd=vel_sd,
+                               azimuth_sd=azimuth_sd, level_sd=level_sd,
+                               gyro_model=gyro_model, accel_model=accel_model)
 
+    result = f.run(trajectory_computed,
+                   observations=[position_obs, ned_velocity_obs,
+                                 body_velocity_obs])
+
+    error = transform.difference_trajectories(result.trajectory, trajectory)
+
+    relative_error = error / result.sd
+    assert (util.compute_rms(relative_error) < 1.6).all()
+
+    gyro_bias_relative_error = (np.abs(result.gyro_estimates.iloc[-1] -
+                                       imu_errors.gyro_bias)
+                                / result.gyro_sd.iloc[-1])
+    assert (gyro_bias_relative_error < 2.0).all()
+
+    accel_bias_relative_error = (np.abs(result.accel_estimates.iloc[-1] -
+                                        imu_errors.accel_bias)
+                                 / result.accel_sd.iloc[-1])
+    assert (accel_bias_relative_error < 2.0).all()
+
+    result = f.run_smoother(trajectory_computed,
+                            observations=[position_obs, ned_velocity_obs,
+                                          body_velocity_obs])
+
+    error = transform.difference_trajectories(result.trajectory, trajectory)
+
+    relative_error = error / result.sd
+    assert (util.compute_rms(relative_error) < 2.0).all()
+
+    gyro_bias_relative_error = np.abs(result.gyro_estimates -
+                                      imu_errors.gyro_bias) / result.gyro_sd
+    assert (gyro_bias_relative_error < 2.0).all(axis=None)
+
+    accel_bias_relative_error = np.abs(result.accel_estimates -
+                                       imu_errors.accel_bias) / result.accel_sd
+    assert (accel_bias_relative_error < 2.0).all(axis=None)
 
 
 if __name__ == '__main__':
