346788cacd453d8b1d2a16975fda556b020d23f9
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2022-05-10 17:11:45 +0500

    MNT: Simplify imports in test_transform.py

--- pyins/tests/test_transform.py
+++ pyins/tests/test_transform.py
@@ -1,27 +1,26 @@
 import numpy as np
 from numpy.testing import assert_allclose, run_module_suite
 from scipy.spatial.transform import Rotation
-from pyins.transform import lla_to_ecef, perturb_lla, phi_to_delta_rph
 from pyins import earth, transform
 
 
 def test_lla_to_ecef():
-    r_e = lla_to_ecef([0, 0, 10])
+    r_e = transform.lla_to_ecef([0, 0, 10])
     assert_allclose(r_e, [earth.R0 + 10, 0, 0])
 
-    r_e = lla_to_ecef([-90, 0, -10])
+    r_e = transform.lla_to_ecef([-90, 0, -10])
     b = (1 - earth.E2) ** 0.5 * earth.R0
     assert_allclose(r_e, [0, 0, -b + 10], atol=1e-9)
 
-    r_e = lla_to_ecef([[0, 0, 10], [-90, 0, -10]])
+    r_e = transform.lla_to_ecef([[0, 0, 10], [-90, 0, -10]])
     assert_allclose(r_e, [[earth.R0 + 10, 0, 0],
                           [0, 0, -b + 10]], atol=1e-9)
 
 
 def test_perturb_ll():
     lla = [40, 50, 0]
-    lla_new = perturb_lla(lla, [10, -20, 5])
-    lla_back = perturb_lla(lla_new, [-10, 20, -5])
+    lla_new = transform.perturb_lla(lla, [10, -20, 5])
+    lla_back = transform.perturb_lla(lla_new, [-10, 20, -5])
     assert_allclose(lla_back, lla, rtol=1e-11)
 
 
@@ -34,7 +33,7 @@ def test_phi_to_delta_rph():
     rph_perturbed = transform.mat_to_rph(mat_perturbed)
     delta_rph_true = rph_perturbed - rph
 
-    T = phi_to_delta_rph(rph)
+    T = transform.phi_to_delta_rph(rph)
     delta_rph_linear = np.rad2deg(T @ phi)
 
     assert_allclose(delta_rph_linear, delta_rph_true, rtol=1e-1)
