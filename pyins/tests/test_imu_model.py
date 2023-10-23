0f76fcb3a52ea2f6299195c1dc35f29278cc0b8e
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2023-10-24 01:10:07 +0500

    MNT: Move InertialSensor into imu_model.py

---
+++ pyins/tests/test_imu_model.py
@@ -0,0 +1,78 @@
+import numpy as np
+from numpy.testing import assert_allclose, assert_equal
+from pyins.imu_model import InertialSensor
+
+
+def test_InertialSensor():
+    s = InertialSensor()
+    assert_equal(s.n_states, 0)
+    assert_equal(s.n_noises, 0)
+    assert_equal(len(s.states), 0)
+    assert_equal(s.P.shape, (0, 0))
+    assert_equal(s.q.shape, (0,))
+    assert_equal(s.F.shape, (0, 0))
+    assert_equal(s.G.shape, (0, 0))
+    assert_equal(s.output_matrix().shape, (3, 0))
+
+    s = InertialSensor(bias=0.1, bias_walk=0.2)
+    assert_equal(s.n_states, 3)
+    assert_equal(s.n_noises, 3)
+    assert_equal(list(s.states.keys()), ['BIAS_1', 'BIAS_2', 'BIAS_3'])
+    assert_equal(list(s.states.values()), [0, 1, 2])
+    assert_allclose(s.P, 0.01 * np.identity(3))
+    assert_equal(s.q, [0.2, 0.2, 0.2])
+    assert_equal(s.F, np.zeros((3, 3)))
+    assert_equal(s.G, np.identity(3))
+    assert_equal(s.output_matrix(), np.identity(3))
+
+    s = InertialSensor(scale=0.2, scale_walk=0.3)
+    assert_equal(s.n_states, 3)
+    assert_equal(s.n_noises, 3)
+    assert_equal(list(s.states.keys()), ['SCALE_1', 'SCALE_2', 'SCALE_3'])
+    assert_equal(list(s.states.values()), [0, 1, 2])
+    assert_allclose(s.P, 0.04 * np.identity(3))
+    assert_equal(s.q, [0.3, 0.3, 0.3])
+    assert_equal(s.F, np.zeros((3, 3)))
+    assert_equal(s.G, np.identity(3))
+    assert_equal(s.output_matrix([1, 2, 3]), np.diag([1, 2, 3]))
+    assert_equal(s.output_matrix([[1, -2, 2], [0.1, 2, 0.5]]),
+                 np.array((np.diag([1, -2, 2]), np.diag([0.1, 2, 0.5]))))
+
+    s = InertialSensor(corr_sd=0.1, corr_time=5)
+    assert_equal(s.n_states, 3)
+    assert_equal(s.n_noises, 3)
+    assert_equal(list(s.states.keys()), ['CORR_1', 'CORR_2', 'CORR_3'])
+    assert_equal(list(s.states.values()), [0, 1, 2])
+    assert_allclose(s.P, 0.01 * np.identity(3))
+    q = 0.1 * (2 / 5) ** 0.5
+    assert_equal(s.q, [q, q, q])
+    assert_allclose(s.F, -np.identity(3) / 5)
+    assert_equal(s.G, np.identity(3))
+
+    s = InertialSensor(bias=0.1, bias_walk=0.2, scale=0.3, scale_walk=0.4,
+                       corr_sd=0.5, corr_time=10)
+    assert_equal(s.n_states, 9)
+    assert_equal(s.n_noises, 9)
+    assert_equal(list(s.states.keys()),
+                 ['BIAS_1', 'BIAS_2', 'BIAS_3', 'SCALE_1', 'SCALE_2',
+                  'SCALE_3', 'CORR_1', 'CORR_2', 'CORR_3'])
+    assert_equal(list(s.states.values()), np.arange(9))
+    assert_allclose(s.P, np.diag([0.01, 0.01, 0.01, 0.09, 0.09, 0.09,
+                                  0.25, 0.25, 0.25]))
+    q_corr = 0.5 * (2 / 10) ** 0.5
+    assert_equal(s.q, [0.2, 0.2, 0.2, 0.4, 0.4, 0.4, q_corr, q_corr, q_corr])
+    assert_allclose(s.F, np.diag([0, 0, 0, 0, 0, 0, -1/10, -1/10, -1/10]))
+    assert_equal(s.G, np.identity(9))
+
+    H = s.output_matrix([1, 2, 3])
+    assert_allclose(H, [[1, 0, 0, 1, 0, 0, 1, 0, 0],
+                        [0, 1, 0, 0, 2, 0, 0, 1, 0],
+                        [0, 0, 1, 0, 0, 3, 0, 0, 1]])
+
+    H = s.output_matrix([[1, 2, 3], [-1, 2, 0.5]])
+    assert_allclose(H[0], [[1, 0, 0, 1, 0, 0, 1, 0, 0],
+                           [0, 1, 0, 0, 2, 0, 0, 1, 0],
+                           [0, 0, 1, 0, 0, 3, 0, 0, 1]])
+    assert_allclose(H[1], [[1, 0, 0, -1, 0, 0, 1, 0, 0],
+                           [0, 1, 0, 0, 2, 0, 0, 1, 0],
+                           [0, 0, 1, 0, 0, 0.5, 0, 0, 1]])
