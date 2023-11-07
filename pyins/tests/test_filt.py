9d445b1328ff46d551ae19c35b625b621e90dca2
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2023-11-08 02:01:20 +0500

    MNT: Remove old FeedbackFilter

--- pyins/tests/test_filt.py
+++ pyins/tests/test_filt.py
@@ -10,66 +10,6 @@ def test_refine_stamps():
     assert_equal(stamps, stamps_true)
 
 
-def test_FeedbackFilter():
-    dt = 0.01
-    rng = np.random.RandomState(123456789)
-
-    trajectory, imu_true = sim.sinusoid_velocity_motion(dt, 300, [50, 60, 100],
-                                                        [1, -1, 0.5], [3, 3, 0.5])
-
-    position_obs = filt.PositionObs(
-        sim.generate_position_observations(trajectory.iloc[::100], 1, rng), 1)
-    ned_velocity_obs = filt.NedVelocityObs(
-        sim.generate_ned_velocity_observations(trajectory.iloc[30::100], 0.5,
-                                               rng), 0.5)
-    body_velocity_obs = filt.BodyVelocityObs(
-        sim.generate_body_velocity_observations(trajectory.iloc[60::100], 0.2,
-                                                rng), 0.2)
-
-    gyro_errors = sim.ImuErrors(
-        bias=np.array([-100, 50, 40]) * transform.DH_TO_RS,
-        noise=1 * transform.DRH_TO_RRS, rng=rng)
-    accel_errors = sim.ImuErrors(bias=[0.1, -0.1, 0.2], noise=1.0 / 60, rng=rng)
-
-    gyro_model = filt.InertialSensor(bias_sd=100 * transform.DH_TO_RS,
-                                     noise=1 * transform.DRH_TO_RRS)
-    accel_model = filt.InertialSensor(bias_sd=0.1, noise=1.0 / 60)
-
-    imu = sim.apply_imu_errors(imu_true, dt, 'increment', gyro_errors, accel_errors)
-    increments = strapdown.compute_theta_and_dv(imu)
-
-    pos_sd = 10
-    vel_sd = 2
-    level_sd = 1.0
-    azimuth_sd = 5.0
-
-    initial = sim.perturb_trajectory_point(trajectory.iloc[0], pos_sd, vel_sd, level_sd,
-                                           azimuth_sd, rng=rng)
-    f = filt.FeedbackFilter(dt, pos_sd=pos_sd, vel_sd=vel_sd,
-                            azimuth_sd=azimuth_sd, level_sd=level_sd,
-                            gyro_model=gyro_model, accel_model=accel_model)
-    integrator = strapdown.Integrator(dt, initial)
-
-    result = f.run(integrator, increments,
-                   observations=[position_obs, ned_velocity_obs, body_velocity_obs],
-                   feedback_period=5)
-
-    error = transform.compute_state_difference(result.trajectory, trajectory)
-
-    relative_error = error / result.sd
-    assert (util.compute_rms(relative_error) < 1.5).all()
-
-    gyro_bias_relative_error = (np.abs(result.gyro_estimates.iloc[-1] -
-                                       gyro_errors.bias)
-                                / result.gyro_sd.iloc[-1])
-    assert (gyro_bias_relative_error < 2.0).all()
-
-    accel_bias_relative_error = (np.abs(result.accel_estimates.iloc[-1] -
-                                        accel_errors.bias)
-                                 / result.accel_sd.iloc[-1])
-    assert (accel_bias_relative_error < 2.0).all()
-
-
 def test_run_feedback_filter():
     dt = 0.01
     rng = np.random.RandomState(123456789)
@@ -113,8 +53,15 @@ def test_run_feedback_filter():
 
     error = transform.compute_state_difference(result.trajectory, trajectory_true)
 
-    relative_error = error / result.trajectory_sd[error.columns]
-    assert (util.compute_rms(relative_error) < 1.5).all()
+    assert (util.compute_rms(error / result.trajectory_sd) < 1.5).all()
+
+    gyro_error = transform.compute_state_difference(result.gyro, gyro_errors.dataframe)
+    assert ((gyro_error.iloc[-1] / result.gyro_sd.iloc[-1]).abs() < 2.0).all()
+
+    accel_error = transform.compute_state_difference(result.accel,
+                                                     accel_errors.dataframe)
+    assert ((accel_error.iloc[-1] / result.accel_sd.iloc[-1]).abs() < 2.0).all()
+
 
 
 def test_FeedforwardFilter():
