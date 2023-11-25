b0ecfb9240f94b250679e3619d51bdce79f56004
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2023-11-25 20:34:06 +0500

    TST: Test filters when with_altitude=False

--- pyins/tests/test_filters.py
+++ pyins/tests/test_filters.py
@@ -1,14 +1,16 @@
 import numpy as np
+import pytest
 from pyins import (inertial_sensor, filters, sim, strapdown, transform, util,
                    measurements)
 
 
-def test_run_feedback_filter():
+@pytest.mark.parametrize("with_altitude", [True, False])
+def test_run_feedback_filter(with_altitude):
     dt = 0.01
     rng = np.random.RandomState(123456789)
 
     trajectory_true, imu_true = sim.sinusoid_velocity_motion(
-        0.5 * dt, 300, [50, 60, 100], [1, -1, 0.5], [3, 3, 0.5], sensor_type='rate')
+        0.5 * dt, 300, [50, 60, 100], [1, -1, 0], [3, 3, 0], sensor_type='rate')
 
     position_obs = measurements.Position(
         sim.generate_position_measurements(trajectory_true.iloc[1::200], 1, rng), 1)
@@ -44,40 +46,48 @@ def test_run_feedback_filter():
     result = filters.run_feedback_filter(
         initial, pos_sd, vel_sd, level_sd, azimuth_sd, increments, gyro_model,
         accel_model, measurements=[position_obs, ned_velocity_obs, body_velocity_obs],
-        time_step=1)
+        time_step=1, with_altitude=with_altitude)
 
     error = transform.compute_state_difference(result.trajectory, trajectory_true)
+    sd = result.trajectory_sd
+    if not with_altitude:
+        error = error.drop(columns=['down', 'VD'])
+        sd = sd.drop(columns=['down', 'VD'])
 
-    assert (util.compute_rms(error / result.trajectory_sd) < 1.5).all()
+    assert (util.compute_rms(error / sd) < 1.5).all()
 
     gyro_error = transform.compute_state_difference(result.gyro, gyro_errors.parameters)
     assert ((gyro_error.iloc[-1] / result.gyro_sd.iloc[-1]).abs() < 2.0).all()
 
     accel_error = transform.compute_state_difference(result.accel,
-                                                     accel_errors.parameters)
-    assert ((accel_error.iloc[-1] / result.accel_sd.iloc[-1]).abs() < 2.0).all()
+                                                     accel_errors.parameters).iloc[-1]
+    accel_sd = result.accel_sd.iloc[-1]
+    if not with_altitude:
+        accel_error = accel_error.iloc[:2]
+        accel_sd = accel_sd.iloc[:2]
+    assert ((accel_error / accel_sd).abs() < 2.0).all()
 
     assert (util.compute_rms(result.innovations['Position']) < 3.0).all()
     assert (util.compute_rms(result.innovations['NedVelocity']) < 3.0).all()
     assert (util.compute_rms(result.innovations['BodyVelocity']) < 3.0).all()
 
 
-def test_run_feedforward_filter():
+@pytest.mark.parametrize("with_altitude", [True, False])
+def test_run_feedforward_filter(with_altitude):
     dt = 0.01
     factor = 5
     rng = np.random.RandomState(123456789)
 
-    trajectory, imu_true = sim.sinusoid_velocity_motion(dt / factor, 300, [50, 60, 100],
-                                                        [1, -1, 0.5], [3, 3, 0.5])
+    trajectory_true, imu_true = sim.sinusoid_velocity_motion(
+        dt / factor, 300, [50, 60, 100], [1, -1, 0], [3, 3, 0])
 
-    position_obs = measurements.Position(
-        sim.generate_position_measurements(trajectory.iloc[1::100 * factor], 1, rng), 1)
-    ned_velocity_obs = measurements.NedVelocity(
-        sim.generate_ned_velocity_measurements(trajectory.iloc[32::100 * factor], 0.5,
-                                               rng), 0.5)
+    position_obs = measurements.Position(sim.generate_position_measurements(
+        trajectory_true.iloc[1::100 * factor], 1, rng), 1)
+    ned_velocity_obs = measurements.NedVelocity(sim.generate_ned_velocity_measurements(
+        trajectory_true.iloc[32::100 * factor], 0.5, rng), 0.5)
     body_velocity_obs = measurements.BodyVelocity(
-        sim.generate_body_velocity_measurements(trajectory.iloc[64::100 * factor], 0.2,
-                                                rng), 0.2)
+        sim.generate_body_velocity_measurements(
+            trajectory_true.iloc[64::100 * factor], 0.2, rng), 0.2)
 
     gyro_errors = inertial_sensor.InertialSensorError(
         bias=np.array([-1, 2, 1]) * transform.DH_TO_RS,
@@ -97,27 +107,33 @@ def test_run_feedforward_filter():
     azimuth_sd = 0.2
 
     pva_error = sim.generate_pva_error(pos_sd, vel_sd, level_sd, azimuth_sd, rng=rng)
-    initial = sim.perturb_pva(trajectory.iloc[0], pva_error)
-    integrator = strapdown.Integrator(initial)
+    # if not with_altitude:
+    #     pva_error[['down', 'VD']] = 0
+    initial = sim.perturb_pva(trajectory_true.iloc[0], pva_error)
+    integrator = strapdown.Integrator(initial, with_altitude)
     trajectory_computed = integrator.integrate(increments)
 
     result = filters.run_feedforward_filter(
-        trajectory.iloc[::factor], trajectory_computed.iloc[::factor],
+        trajectory_true.iloc[::factor], trajectory_computed.iloc[::factor],
         pos_sd, vel_sd, level_sd, azimuth_sd, gyro_model, accel_model,
-        measurements=[position_obs, ned_velocity_obs, body_velocity_obs], time_step=1)
+        measurements=[position_obs, ned_velocity_obs, body_velocity_obs], time_step=1,
+        with_altitude=with_altitude)
 
-    error = transform.compute_state_difference(result.trajectory, trajectory)
+    error = transform.compute_state_difference(result.trajectory, trajectory_true)
+    sd = result.trajectory_sd
+    if not with_altitude:
+        error = error.drop(columns=['down', 'VD'])
+        sd = sd.drop(columns=['down', 'VD'])
 
-    relative_error = error / result.trajectory_sd
-    assert (util.compute_rms(relative_error) < 1.6).all()
+    assert (util.compute_rms(error / sd) < (1.6 if with_altitude else 2.2)).all()
 
     gyro_bias_relative_error = (np.abs(result.gyro.iloc[-1] - gyro_errors.bias)
                                 / result.gyro_sd.iloc[-1])
-    assert (gyro_bias_relative_error < 2.0).all()
+    assert (gyro_bias_relative_error < 3.0).all()
 
-    accel_bias_relative_error = (np.abs(result.accel.iloc[-1] - accel_errors.bias)
-                                 / result.accel_sd.iloc[-1])
-    assert (accel_bias_relative_error < 2.0).all()
+    accel_error = transform.compute_state_difference(result.accel,
+                                                     accel_errors.parameters).iloc[-1]
+    assert ((accel_error / result.accel_sd.iloc[-1]).abs() < 2.1).all()
 
     assert (util.compute_rms(result.innovations['Position']) < 3.0).all()
     assert (util.compute_rms(result.innovations['NedVelocity']) < 3.0).all()
