a7d7fd70c9559a68ffbae1551090d0c9d8859eac
MyFunnyCat <>
2023-12-01 00:16:45 +0300

    [TEST] fix flaws

--- pyins/tests/test_numba_integrate.py
+++ pyins/tests/test_numba_integrate.py
@@ -6,25 +6,23 @@ from pyins._numba_integrate import gravity, mat_from_rotvec
 
 
 def test_earth():
-    assert_allclose(gravity(0, 100), earth.gravity(0, 100))
-    assert_allclose(gravity(90, -100), earth.gravity(90, -100))
-    assert_allclose(gravity(-90, 1000), earth.gravity(-90, 1000))
-
+    np.random.seed(0)
     for i in range(10):
-        lat, alt = [180, 10000] * (np.random.rand(2) - [90, 0])
+        lat, alt = [90, 500] * (np.random.rand(2) - [180, 1000])
         assert_allclose(gravity(lat, alt), earth.gravity(lat, alt))
 
 
 def test_mat_from_rotvec():
-    m_ref = Rotation.from_rotvec([0, 0, 0]).as_matrix()
-    m_test = np.zeros((3, 3))
+    mat_ref = np.eye(3)
+    mat_test = np.zeros((3, 3))
     rv = np.asarray([0, 0, 0])
-    mat_from_rotvec(rv, m_test)
-    assert_allclose(m_test, m_ref)
+    mat_from_rotvec(rv, mat_test)
+    assert_allclose(mat_ref, mat_ref)
 
+    np.random.seed(0)
     for i in range(10):
         rv = np.random.randn(3)
-        m_ref = Rotation.from_rotvec(rv).as_matrix()
-        m_test = np.zeros((3, 3))
-        mat_from_rotvec(rv, m_test)
-        assert_allclose(m_test, m_ref)
+        mat_ref = Rotation.from_rotvec(rv).as_matrix()
+        mat_test = np.zeros((3, 3))
+        mat_from_rotvec(rv, mat_test)
+        assert_allclose(mat_test, mat_ref)
