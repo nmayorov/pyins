02a7d6ffd5331a65bc945431436d8f837e504097
MyFunnyCat <>
2023-12-01 01:21:32 +0300

    [TEST] add test for util.mm_prod

--- pyins/tests/test_util.py
+++ pyins/tests/test_util.py
@@ -3,6 +3,25 @@ from numpy.testing import assert_allclose
 from pyins import util
 
 
+def test_mm_prod():
+    np.random.seed(0)
+    a = np.random.randn(3, 4)
+    b = np.random.randn(4, 3)
+    assert_allclose(util.mm_prod(a, b), a @ b)
+    assert_allclose(util.mm_prod(a, b, at=True, bt=True), a.T @ b.T)
+
+    a = np.random.randn(10, 3, 4)
+    at = a.transpose((0, 2, 1))
+    assert_allclose(util.mm_prod(a, b), a @ b)
+    # crashes
+    # assert_allclose(util.mm_prod(a, b, at=True, bt=True), at @ b.T)
+
+    b = np.random.randn(10, 4, 3)
+    bt = b.transpose((0, 2, 1))
+    assert_allclose(util.mm_prod(a, b), a @ b)
+    assert_allclose(util.mm_prod(a, b, at=True, bt=True), at @ bt)
+
+
 def test_skew_matrix():
     vec = np.array([
         [0, 1, 2],
