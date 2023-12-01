31488884825b873a43b3012531a7f5814d866595
MyFunnyCat <>
2023-12-01 23:39:25 +0300

    [BUG] fix test_numba_integrate.py

--- pyins/tests/test_numba_integrate.py
+++ pyins/tests/test_numba_integrate.py
@@ -3,16 +3,18 @@ from numpy.testing import assert_allclose
 from scipy.spatial.transform import Rotation
 from pyins import earth
 from pyins._numba_integrate import gravity, mat_from_rotvec
+import pytest
 
 
-def test_earth():
+def test_gravity():
     np.random.seed(0)
     for i in range(10):
-        lat, alt = [90, 500] * (np.random.rand(2) - [180, 1000])
+        lat, alt = [180, 1000] * np.random.rand(2) - [90, 500]
         assert_allclose(gravity(lat, alt), earth.gravity(lat, alt))
 
 
-def test_mat_from_rotvec():
+@pytest.mark.parametrize("sd", [1.0, 0.01])
+def test_mat_from_rotvec(sd):
     mat_ref = np.eye(3)
     mat_test = np.zeros((3, 3))
     rv = np.asarray([0, 0, 0])
@@ -21,7 +23,7 @@ def test_mat_from_rotvec():
 
     np.random.seed(0)
     for i in range(10):
-        rv = np.random.randn(3)
+        rv = sd * np.random.randn(3)
         mat_ref = Rotation.from_rotvec(rv).as_matrix()
         mat_test = np.zeros((3, 3))
         mat_from_rotvec(rv, mat_test)
