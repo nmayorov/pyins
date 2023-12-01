98a3a841fdacfab39761891387ca1a6f5559acf0
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2023-12-01 06:47:07 +0500

    TST: Minor cleanup of test_EstimationModel

--- pyins/tests/test_inertial_sensor.py
+++ pyins/tests/test_inertial_sensor.py
@@ -5,69 +5,69 @@ from pyins.inertial_sensor import EstimationModel, Parameters
 
 
 def test_EstimationModel():
-    s = EstimationModel()
-    assert_equal(s.n_states, 0)
-    assert_equal(s.n_noises, 0)
-    assert_equal(s.n_output_noises, 0)
-    assert_equal(len(s.states), 0)
-    assert_equal(s.P.shape, (0, 0))
-    assert_equal(s.q.shape, (0,))
-    assert_equal(s.v, [])
-    assert_equal(s.F.shape, (0, 0))
-    assert_equal(s.G.shape, (0, 0))
-    assert_equal(s.output_matrix().shape, (3, 0))
+    model = EstimationModel()
+    assert_equal(model.n_states, 0)
+    assert_equal(model.n_noises, 0)
+    assert_equal(model.n_output_noises, 0)
+    assert_equal(model.states, [])
+    assert_equal(model.P.shape, (0, 0))
+    assert_equal(model.q, [])
+    assert_equal(model.v, [])
+    assert_equal(model.F.shape, (0, 0))
+    assert_equal(model.G.shape, (0, 0))
+    assert_equal(model.J, [[], [], []])
+    assert_equal(model.output_matrix(), [[], [], []])
 
-    s = EstimationModel(bias_sd=0.1, bias_walk=0.2)
-    assert_equal(s.n_states, 3)
-    assert_equal(s.n_noises, 3)
-    assert_equal(s.n_output_noises, 0)
-    assert_equal(s.states, ['bias_x', 'bias_y', 'bias_z'])
-    assert_allclose(s.P, 0.01 * np.identity(3))
-    assert_equal(s.q, [0.2, 0.2, 0.2])
-    assert_equal(s.v, [])
-    assert_equal(s.F, np.zeros((3, 3)))
-    assert_equal(s.G, np.identity(3))
-    assert_equal(s.J, np.empty((3, 0)))
-    assert_equal(s.output_matrix(), np.identity(3))
+    model = EstimationModel(bias_sd=0.1, bias_walk=0.2)
+    assert_equal(model.n_states, 3)
+    assert_equal(model.n_noises, 3)
+    assert_equal(model.n_output_noises, 0)
+    assert_equal(model.states, ['bias_x', 'bias_y', 'bias_z'])
+    assert_allclose(model.P, 0.01 * np.identity(3))
+    assert_equal(model.q, [0.2, 0.2, 0.2])
+    assert_equal(model.v, [])
+    assert_equal(model.F, np.zeros((3, 3)))
+    assert_equal(model.G, np.identity(3))
+    assert_equal(model.J, [[], [], []])
+    assert_equal(model.output_matrix(), np.identity(3))
 
-    s = EstimationModel(bias_sd=[0.1, 0.0, 0.2], bias_walk=[0.0, 0.0, 0.01],
-                        noise=[0.0, 0.02, 0.0])
-    assert_equal(s.n_states, 2)
-    assert_equal(s.n_noises, 1)
-    assert_equal(s.n_output_noises, 1)
-    assert_equal(s.states, ['bias_x', 'bias_z'])
-    assert_allclose(s.P, np.diag([0.1**2, 0.2**2]))
-    assert_equal(s.q, [0.01])
-    assert_equal(s.v, [0.02])
-    assert_equal(s.F, np.zeros((2, 2)))
-    assert_equal(s.G, [[0], [1]])
-    assert_equal(s.J, [[0], [1], [0]])
-    assert_equal(s.output_matrix(), [[1, 0], [0, 0], [0, 1]])
+    model = EstimationModel(bias_sd=[0.1, 0.0, 0.2], bias_walk=[0.0, 0.0, 0.01],
+                            noise=[0.0, 0.02, 0.0])
+    assert_equal(model.n_states, 2)
+    assert_equal(model.n_noises, 1)
+    assert_equal(model.n_output_noises, 1)
+    assert_equal(model.states, ['bias_x', 'bias_z'])
+    assert_allclose(model.P, np.diag([0.1**2, 0.2**2]))
+    assert_equal(model.q, [0.01])
+    assert_equal(model.v, [0.02])
+    assert_equal(model.F, np.zeros((2, 2)))
+    assert_equal(model.G, [[0], [1]])
+    assert_equal(model.J, [[0], [1], [0]])
+    assert_equal(model.output_matrix(), [[1, 0], [0, 0], [0, 1]])
 
-    s = EstimationModel(bias_sd=0.1, bias_walk=0.2,
-                        scale_misal_sd=np.diag([0.3, 0.3, 0.3]))
-    assert_equal(s.n_states, 6)
-    assert_equal(s.n_noises, 3)
-    assert_equal(s.n_output_noises, 0)
-    assert_equal(s.states, ['bias_x', 'bias_y', 'bias_z', 'sm_xx', 'sm_yy', 'sm_zz'])
-    assert_allclose(s.P, np.diag([0.01, 0.01, 0.01, 0.09, 0.09, 0.09]))
-    assert_equal(s.q, [0.2, 0.2, 0.2])
-    assert_equal(s.v, [])
-    assert_allclose(s.F, np.zeros((6, 6)))
-    assert_equal(s.G, np.vstack([np.identity(3), np.zeros((3, 3))]))
-
-    H = s.output_matrix([1, 2, 3])
-    assert_allclose(H, [[1, 0, 0, 1, 0, 0],
-                        [0, 1, 0, 0, 2, 0],
-                        [0, 0, 1, 0, 0, 3]])
-
-    H = s.output_matrix([[1, 2, 3], [-1, 2, 0.5]])
-    assert_allclose(H[0], [[1, 0, 0, 1, 0, 0],
-                           [0, 1, 0, 0, 2, 0],
-                           [0, 0, 1, 0, 0, 3]])
-    assert_allclose(H[1], [[1, 0, 0, -1, 0, 0],
-                           [0, 1, 0, 0, 2, 0],
-                           [0, 0, 1, 0, 0, 0.5]])
+    model = EstimationModel(bias_sd=0.1, bias_walk=0.2,
+                            scale_misal_sd=np.diag([0.3, 0.3, 0.3]))
+    assert_equal(model.n_states, 6)
+    assert_equal(model.n_noises, 3)
+    assert_equal(model.n_output_noises, 0)
+    assert_equal(model.states, ['bias_x', 'bias_y', 'bias_z', 'sm_xx', 'sm_yy', 'sm_zz'])
+    assert_allclose(model.P, np.diag([0.01, 0.01, 0.01, 0.09, 0.09, 0.09]))
+    assert_equal(model.q, [0.2, 0.2, 0.2])
+    assert_equal(model.v, [])
+    assert_allclose(model.F, np.zeros((6, 6)))
+    assert_equal(model.G, np.vstack([np.identity(3), np.zeros((3, 3))]))
+    assert_equal(model.J, [[], [], []])
+    assert_allclose(model.output_matrix([1,2, 3]), [[1, 0, 0, 1, 0, 0],
+                                                    [0, 1, 0, 0, 2, 0],
+                                                    [0, 0, 1, 0, 0, 3]])
+    assert_allclose(model.output_matrix([[1, 2, 3], [-1, 2, 0.5]]),
+                    [[[1, 0, 0, 1, 0, 0],
+                      [0, 1, 0, 0, 2, 0],
+                      [0, 0, 1, 0, 0, 3]],
+                     [[1, 0, 0, -1, 0, 0],
+                      [0, 1, 0, 0, 2, 0],
+                      [0, 0, 1, 0, 0, 0.5]]
+                     ])
 
 
 def test_Parameters():
