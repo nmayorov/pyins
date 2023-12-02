bec2882b867f27847a94cfa5d1da7d2e21a427d7
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2023-12-02 08:11:01 +0500

    TST: Cleanup test_sim_on_stationary

--- pyins/tests/test_sim.py
+++ pyins/tests/test_sim.py
@@ -1,9 +1,7 @@
 import numpy as np
-import pandas as pd
-from numpy.testing import assert_allclose
+from numpy.testing import assert_allclose, assert_array_equal
 from pyins import sim
 from pyins import earth
-from pyins.inertial_sensor import EstimationModel
 from pyins.util import GYRO_COLS, ACCEL_COLS
 
 
@@ -17,34 +15,27 @@ def test_sim_on_stationary():
     lla[:, 0] = 50.0
     lla[:, 1] = 45.0
     lla[:, 2] = 0.0
-    hpr = np.zeros((n_points, 3))
+    rph = np.zeros((n_points, 3))
     V_n = np.zeros((n_points, 3))
 
-    slat = np.sin(np.deg2rad(50))
-    clat = (1 - slat**2) ** 0.5
-
-    gyro = earth.RATE * np.array([clat, 0, -slat])
-    accel = np.array([0, 0, -earth.gravity(50, 0)])
+    gyro = earth.rate_n(50)
+    accel = -earth.gravity_n(50, 0)
 
     for lla_arg, velocity_n_arg in [(lla[0], V_n), (lla, V_n), (lla, None)]:
         for sensor_type in ['rate', 'increment']:
-            trajectory, imu = sim.generate_imu(time, lla_arg, hpr, velocity_n_arg,
+            trajectory, imu = sim.generate_imu(time, lla_arg, rph, velocity_n_arg,
                                                sensor_type=sensor_type)
-
+            assert_array_equal(trajectory.index, time)
             assert_allclose(trajectory.lat, 50, rtol=1e-12)
             assert_allclose(trajectory.lon, 45, rtol=1e-12)
+            assert_allclose(trajectory.alt, 0, atol=1e-30)
             assert_allclose(trajectory.VE, 0, atol=1e-7)
             assert_allclose(trajectory.VN, 0, atol=1e-7)
+            assert_allclose(trajectory.VD, 0, atol=1e-7)
             assert_allclose(trajectory.roll, 0, atol=1e-8)
             assert_allclose(trajectory.pitch, 0, atol=1e-8)
             assert_allclose(trajectory.heading, 0, atol=1e-8)
-
-            accel_atol = 1e-7 if sensor_type == 'increment' else 1e-6
             factor = dt if sensor_type == 'increment' else 1
-
-            gyro_g = imu[GYRO_COLS].values
-            accel_g = imu[ACCEL_COLS].values
-
-            for i in range(3):
-                assert_allclose(gyro_g[:, i], gyro[i] * factor, atol=1e-14)
-                assert_allclose(accel_g[:, i], accel[i] * factor, atol=accel_atol)
+            assert_allclose(imu[GYRO_COLS] - gyro * factor, 0, atol=1e-14)
+            assert_allclose(imu[ACCEL_COLS] - accel * factor, 0,
+                            atol=1e-7 if sensor_type == 'increment' else 1e-6)
