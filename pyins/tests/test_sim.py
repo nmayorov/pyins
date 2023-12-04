6ac8b62b0f64f8062e8ef1c929c446018c42d823
Nikolay Mayorov <nikolay.mayorov@zoho.com>
2023-12-04 12:41:31 +0500

    TST: Add test_generate_sine_velocity_motion

--- pyins/tests/test_sim.py
+++ pyins/tests/test_sim.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.testing import assert_allclose, assert_array_equal
-from pyins import earth, sim, transform
+from pyins import earth, sim, transform, util
 from pyins.util import GYRO_COLS, ACCEL_COLS
 
 
@@ -40,3 +40,25 @@ def test_generate_imu():
             assert_allclose(imu[GYRO_COLS] - gyro * factor, 0, atol=1e-14)
             assert_allclose(imu[ACCEL_COLS] - accel * factor, 0,
                             atol=1e-7 if sensor_type == 'increment' else 1e-6)
+
+
+def test_generate_sine_velocity_motion():
+    trajectory, imu = sim.generate_sine_velocity_motion(
+        0.1, 10.1, [55, 37, 0], 0, [1, 1, 0], 10)
+    assert_allclose(trajectory.lat.iloc[0], trajectory.lat.iloc[-1])
+    assert_allclose(trajectory.lon.iloc[0], trajectory.lon.iloc[-1])
+    assert_array_equal(trajectory.alt, 0)
+    assert_allclose(trajectory[['VN', 'VE']].max(), 1)
+    assert_allclose(trajectory[['VN', 'VE']].min(), -1)
+    assert_allclose(trajectory.VN.iloc[0], trajectory.VN.iloc[-1], atol=1e-15)
+    assert_allclose(trajectory.VE.iloc[0], trajectory.VE.iloc[-1])
+    assert_array_equal(trajectory.VD, 0)
+    assert_array_equal(trajectory[['roll', 'pitch']], 0)
+    assert_allclose(
+        util.to_180_range(trajectory.heading - (90 - 360 * trajectory.index / 10)), 0,
+        atol=1e-13)
+    assert_allclose(imu[['gyro_x', 'gyro_y']].mean(), 0, atol=1e-6)
+    assert_allclose(imu.gyro_z, -360 / 10 * transform.DEG_TO_RAD, rtol=1e-4)
+    assert_allclose(imu.accel_x, 0, atol=1e-4)
+    assert_allclose(imu.accel_y, -360 / 10 * transform.DEG_TO_RAD, rtol=1e-3)
+    assert_allclose(imu.accel_z, -earth.gravity(55, 0), rtol=1e-5)
