da1914957fe20f61780e5346c4e206c737612889
MyFunnyCat <>
2023-12-09 14:21:31 +0300

    [STY] better names in test_transform.py

--- pyins/tests/test_transform.py
+++ pyins/tests/test_transform.py
@@ -3,10 +3,11 @@ from numpy.testing import assert_allclose
 import pandas as pd
 from scipy.spatial.transform import Rotation
 from pyins import earth, transform
-from pyins.sim import sinusoid_velocity_motion
+from pyins.sim import generate_sine_velocity_motion
 from pyins import util
 from pyins.util import (LLA_COLS, NED_COLS, VEL_COLS, RPH_COLS,
                         GYRO_COLS, RATE_COLS)
+
 
 def test_lla_to_ecef():
     r_e = transform.lla_to_ecef([0, 0, 10])
@@ -25,28 +26,28 @@ def test_lla_to_ned():
     ned = transform.lla_to_ned(lla)
     a = earth.A + 1000
     b = (1 - earth.E2) ** 0.5 * earth.A
-    test = [[0, 0, 0], [b, 0, a], [0, -earth.A + 1000, a]]
-    assert_allclose(ned, test, atol=1e-8)
-    assert(isinstance(ned, np.ndarray))
+    expected = [[0, 0, 0], [b, 0, a], [0, -earth.A + 1000, a]]
+    assert_allclose(ned, expected, atol=1e-8)
+    assert isinstance(ned, np.ndarray)
 
     lla = pd.DataFrame(data=lla, columns=LLA_COLS)
     ned = transform.lla_to_ned(lla)
-    test = pd.DataFrame(data=test, columns=NED_COLS)
-    assert_allclose(ned, test, atol=1e-8)
-    assert(isinstance(ned, pd.DataFrame))
+    expected = pd.DataFrame(data=expected, columns=NED_COLS)
+    assert_allclose(ned, expected, atol=1e-8)
+    assert isinstance(ned, pd.DataFrame)
 
     lla = [[0, 0, 1000], [90, 90, 0], [0, -90, -1000]]
     ned = transform.lla_to_ned(lla, [0, -90, -1000])
     a = earth.A - 1000
-    test = [[0, earth.A + 1000, a], [b, 0, a], [0, 0, 0]]
-    assert_allclose(ned, test, atol=1e-8)
-    assert(isinstance(ned, np.ndarray))
+    expected = [[0, earth.A + 1000, a], [b, 0, a], [0, 0, 0]]
+    assert_allclose(ned, expected, atol=1e-8)
+    assert isinstance(ned, np.ndarray)
 
     lla = pd.DataFrame(data=lla, columns=LLA_COLS)
     ned = transform.lla_to_ned(lla, [0, -90, -1000])
-    test = pd.DataFrame(data=test, columns=NED_COLS)
-    assert_allclose(ned, test, atol=1e-8)
-    assert(isinstance(ned, pd.DataFrame))
+    expected = pd.DataFrame(data=expected, columns=NED_COLS)
+    assert_allclose(ned, expected, atol=1e-8)
+    assert isinstance(ned, pd.DataFrame)
 
 
 def test_perturb_ll():
@@ -62,20 +63,20 @@ def test_perturb_ll():
 
 
 def test_translate_trajectory():
-    traj, imu = sinusoid_velocity_motion(0.1, 60, [10, 20, -3], [5, 7, 3])
+    traj, imu = generate_sine_velocity_motion(0.1, 60, [10, 20, -3], [5, 7, 3])
     traj_new = transform.translate_trajectory(traj, [10, -20, 5])
-    mat = Rotation.from_euler('xyz', traj[RPH_COLS], degrees=True).as_matrix()
-    ned = util.mv_prod(mat, [10, -20, 5])
-    lla_test = transform.perturb_lla(traj[LLA_COLS], ned)
-    assert_allclose(traj_new[LLA_COLS], lla_test, rtol=1e-11)
+    mat_nb = Rotation.from_euler('xyz', traj[RPH_COLS], degrees=True).as_matrix()
+    ned = util.mv_prod(mat_nb, [10, -20, 5])
+    lla = transform.perturb_lla(traj[LLA_COLS], ned)
+    assert_allclose(traj_new[LLA_COLS], lla, rtol=1e-11)
     assert_allclose(traj_new[VEL_COLS], traj[VEL_COLS], rtol=1e-11)
     assert_allclose(traj_new[RPH_COLS], traj[RPH_COLS], rtol=1e-11)
 
     traj[RATE_COLS] = imu[GYRO_COLS].values
     traj_new = transform.translate_trajectory(traj, [10, -20, 5])
     vel_b = np.cross(traj[RATE_COLS], [10, -20, 5])
-    vel_test = traj[VEL_COLS] + util.mv_prod(mat, vel_b)
-    assert_allclose(traj_new[VEL_COLS], vel_test)
+    vel = traj[VEL_COLS] + util.mv_prod(mat_nb, vel_b)
+    assert_allclose(traj_new[VEL_COLS], vel)
 
     traj_back = transform.translate_trajectory(traj_new, [-10, 20, -5])
     assert_allclose(traj_back, traj)
@@ -94,10 +95,10 @@ def test_compute_lla_difference():
 
 
 def test_resample_state():
-    traj, _ = sinusoid_velocity_motion(0.02, 60.01, [40, -50, 100], [5, -7, 0],
-                                       velocity_change_amplitude=[10, 15, 2])
-    ref_traj, _ = sinusoid_velocity_motion(0.01, 60.01, [40, -50, 100], [5, -7, 0],
-                                           velocity_change_amplitude=[10, 15, 2])
+    traj, _ = generate_sine_velocity_motion(0.02, 60.01, [40, -50, 100], [5, -7, 0],
+                                            velocity_change_amplitude=[10, 15, 2])
+    ref_traj, _ = generate_sine_velocity_motion(0.01, 60.01, [40, -50, 100], [5, -7, 0],
+                                                velocity_change_amplitude=[10, 15, 2])
     test_traj = transform.resample_state(traj, ref_traj.index)
 
     assert_allclose(test_traj.lat, ref_traj.lat)
@@ -106,6 +107,22 @@ def test_resample_state():
 
     assert_allclose(test_traj[VEL_COLS], ref_traj[VEL_COLS], atol=1e-5)
     assert_allclose(test_traj[RPH_COLS], ref_traj[RPH_COLS], atol=3e-4)
+
+
+def test_compute_state_difference():
+    traj1, _ = generate_sine_velocity_motion(0.02, 60.01, [40, -50, 100], [5, -7, 0],
+                                             velocity_change_amplitude=[10, 15, 2])
+    traj2, _ = generate_sine_velocity_motion(0.01, 60.01, [40, -50, 100], [5, -7, 0],
+                                             velocity_change_amplitude=[10, 15, 2])
+    traj2 = traj2.iloc[1::2]
+    traj2[LLA_COLS] = transform.perturb_lla(traj2[LLA_COLS], [-3, 5, 7])
+    traj2[VEL_COLS] += [1, -2, 3]
+    traj2[RPH_COLS] += [359, 3, -3]
+
+    diff = transform.compute_state_difference(traj1, traj2)
+    assert_allclose(diff[NED_COLS] - [3, -5, -7], 0, atol=1e-4)
+    assert_allclose(diff[VEL_COLS] - [-1, 2, -3], 0, atol=1e-5)
+    assert_allclose(diff[RPH_COLS] - [1, -3, 3], 0, atol=3e-4)
 
 
 def test_mat_en_from_ll():
